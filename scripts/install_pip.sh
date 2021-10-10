0a000d3f001cea77f5816f83ea2d5c100aa41d63
JunukCha <jucha@unist.ac.kr>
2021-10-11 00:16:03 +0900

    scripts

---
+++ scripts/install_pip.sh
@@ -0,0 +1,4 @@
+conda create -n SSPSE python=3.7
+
+conda activate SSPSE
+pip install -r requirements.txt
---
+++ scripts/prepare_data.sh
@@ -0,0 +1,5 @@
+mkdir -p data
+cd data
+gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
+unzip vibe_data.zip
+rm vibe_data.zip
