759b12e796671efe46f34dd05393dbc0e3cc335f
JunukCha <jucha@unist.ac.kr>
2021-10-11 00:31:25 +0900

    scripts

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -10,3 +10,7 @@ rm dataset_extras.zip
 gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
 unzip vibe_data.zip
 rm vibe_data.zip
+
+### Other data
+gdown "https://drive.google.com/uc?id=1z3YftRaQvEmxb_f-q_Y63CEDwVCjBxHu"
+gdown "https://drive.google.com/uc?id=1DdhMs5PFZMY74-7cffhmBJI5wzyTdnGA"
